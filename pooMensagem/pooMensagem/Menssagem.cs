dbd4b8ee595266e4c4c1ff0c6066bb05a81a4df5
Bella <barbaramy_@hotmail.com>
2022-01-13 12:08:08 -0300

    Exercicios

---
+++ pooMensagem/pooMensagem/Menssagem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    public class Menssagem
+    {
+        public String textoMenssagem;
+
+
+
+        public void exibirMensagem()
+        {
+            Console.WriteLine(this.textoMenssagem);
+        }
+    }
+}
---
+++ pooMensagem/pooMensagem/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMensagem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menssagem msg1, msg2;
+            msg1 = new Menssagem();
+            msg1.textoMenssagem = "Olá querida";
+            msg1.exibirMensagem();
+
+            msg2 = new Menssagem();
+            msg2.textoMenssagem = "Buffo querida";
+            msg2.exibirMensagem();
+
+        }
+    }
+}
