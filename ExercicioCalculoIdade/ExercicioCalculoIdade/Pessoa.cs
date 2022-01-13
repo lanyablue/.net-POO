3de68c511ecdeb2dbe324c6c8c791e906195e4a4
Bella <barbaramy_@hotmail.com>
2022-01-13 16:15:45 -0300

    novo exercicio

---
+++ ExercicioCalculoIdade/ExercicioCalculoIdade/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCalculoIdade
+{
+    public class Pessoa
+    {
+        private int anoNascimento;
+        public int AnoNascimento
+        {
+            
+            get { return this.anoNascimento; }
+            set { this.anoNascimento = value; }
+
+        }
+
+
+        private string nome;
+        public string Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value.ToUpper(); }
+        }
+
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("Nome: " + this.nome);
+            Console.WriteLine("Ano de nascimento: " + anoNascimento);
+            int idade = this.CalcularIdade(); 
+            Console.WriteLine("Idade:  " + idade);
+        }
+
+        private int CalcularIdade()
+        {
+            DateTime data =  DateTime.Now;
+            int ano = data.Year;
+            int idade = ano - this.anoNascimento;
+            return idade;
+        }
+    }
+}
---
+++ ExercicioCalculoIdade/ExercicioCalculoIdade/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioCalculoIdade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Programa que Calcula a idade de uma pessoa: ");
+                Pessoa p = new Pessoa();
+                Console.WriteLine("Nome da pessoa: ");
+                p.Nome = Console.ReadLine();
+                Console.WriteLine("Ano de nascimento: ");
+                p.AnoNascimento = Convert.ToInt32(Console.ReadLine());
+                p.ExibirDados();
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
