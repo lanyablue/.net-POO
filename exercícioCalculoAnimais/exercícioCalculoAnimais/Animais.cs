33243cbe204b253c9c11c2ce6fc3867f61413b0f
Bella <barbaramy_@hotmail.com>
2022-01-14 12:29:56 -0300

    Novo exericico

---
+++ exercícioCalculoAnimais/exercícioCalculoAnimais/Animais.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercícioCalculoAnimais
+{
+    internal class Animais
+    {
+        private String nome;
+        private String tipo;
+
+
+        public String Nome
+        {
+            get { return this.nome; }
+            set { this.nome = value.ToUpper(); }
+        }
+
+
+        public String Tipo
+        {
+            get { return this.tipo; }
+            set
+            {
+                if (value == "Cachorro" || value == "Gato" || value == "Peixe")
+                {
+                    this.tipo = value;
+                }
+                else
+                {
+                    this.tipo = "Peixe";
+                }
+            }
+        }
+
+
+
+        }
+}
---
+++ exercícioCalculoAnimais/exercícioCalculoAnimais/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercícioCalculoAnimais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Animais a1 = new Animais();
+            Animais a2 = new Animais();
+
+            int quantCachorro = 0, quantGato = 0, quantPeixe = 0;
+
+            // Primeiro animal
+            Console.WriteLine("Controle de animais");
+            Console.WriteLine("Informe o nome do animal:");
+            a1.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o tipo do animal (Gato, Cachorro ou Peixe):");
+            a1.Tipo = Console.ReadLine();
+
+            if (a1.Tipo == "Cachorro") quantCachorro++;
+            if (a1.Tipo == "Gato") quantGato++;
+            if (a1.Tipo == "Peixe") quantPeixe++;
+
+
+            // Segundo animail
+            Console.WriteLine("Controle de animais");
+            Console.WriteLine("Informe o nome do animal:");
+            a2.Nome = Console.ReadLine();
+            Console.WriteLine("Informe o tipo do animal (Gato, Cachorro ou Peixe):");
+            a2.Tipo = Console.ReadLine();
+
+            if (a2.Tipo == "Cachorro") quantCachorro++;
+            if (a2.Tipo == "Gato") quantGato++;
+            if (a2.Tipo == "Peixe") quantPeixe++;
+
+
+
+            Console.WriteLine("Total de animais:");
+            Console.WriteLine("Cachorros: " + quantCachorro);
+            Console.WriteLine("Gatos: " + quantGato);
+            Console.WriteLine("Peixes: " + quantPeixe);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
