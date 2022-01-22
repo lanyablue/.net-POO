637f8a0b13ff93b4e085c2b1813cf40e8c037161
Bella <barbaramy_@hotmail.com>
2022-01-22 19:14:07 -0300

    exercice

---
+++ codecamp/phoneNumber/phoneNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Write a function that accepts an array of 10 integers (between 0 and 9), 
+ * that returns a string of those numbers in the form of a phone number.*/
+
+
+namespace phoneNumber
+{
+    internal class Program
+    {
+        public static string phone(int[] numbers)
+        {
+
+            return $"({numbers[0]}{numbers[1]}{numbers[2]}) {numbers[3]}{numbers[3]}{numbers[5]} - {numbers[6]}{numbers[7]}{numbers[8]}{numbers[9]}";
+        }
+
+       
+
+    }
+}
+
